128d09fed6af70466a2744ae3155badb70175444
Asif-1331 <asifahmmed1331@gmail.com>
2023-12-20 14:30:08 +0600

    added main boilerplate files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+    <div className='flex p-3 gap-x-2'>
+      <input type="text" className='border border-gray-500' />
+      <button>Add Item</button>
+    </div>
+    </>
+  )
+}
+
+export default App
